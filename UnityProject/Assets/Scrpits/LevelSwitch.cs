dc93cb2ca143cc4f2e86dedcda632aa9c5bcc855
Alex Merritt <andermerritt@gmail.com>
2014-05-09 21:01:37 -0700

    Main Menu
    added a main menu for the game

---
+++ UnityProject/Assets/Scrpits/LevelSwitch.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSwitch : MonoBehaviour 
+{
+    public enum LevelCode
+    {
+        Exit,
+        MainMenu,
+        Game
+    }
+    public KeyCode Advance;
+    public KeyCode Exit;
+
+    public LevelCode NextLevel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(Advance))
+        {
+            //Application.LoadLevel("testscene");
+            //SwitchScene();
+        }
+        if (Input.GetKeyDown(Exit))
+        {
+            Application.Quit();
+        }
+	}
+
+    public void SwitchScene()
+    {
+        if (NextLevel == LevelCode.Exit)
+        {
+            Application.Quit();
+        }
+
+        else if (NextLevel == LevelCode.MainMenu)
+        {
+        }
+
+    }
+}
